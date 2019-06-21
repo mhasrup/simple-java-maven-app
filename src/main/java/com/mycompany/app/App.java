f4f969eadc8aa053141962ebaa8835736afcb425
mhasrup <mhasrup@gmail.com>
2019-06-21 16:21:47 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello Laxman in Teamcity!";
+    private final String message = "Hello Laxman in Teamcity feature brach!";
 
     public App() {}
 
